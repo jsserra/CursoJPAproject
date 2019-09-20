449e6e9dc754f66cfc19b8b9b4adabeb69fdc4eb
Juliano Sales <jsserra21@gmail.com>
2019-09-19 22:19:44 -0300

    Project created

---
+++ src/main/java/com/udemy/curso/CursoJpAprojectApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJpAprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJpAprojectApplication.class, args);
+	}
+
+}
